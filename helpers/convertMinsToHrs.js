d8e400924a26417711da390fb35bc03e05f5c04c
Eugene Wong <gnow.enegue@gmail.com>
2019-12-13 01:21:17 +0800

    Added validation for input of function `convertMinsToHrs` function.

--- helpers/convertMinsToHrs.js
+++ helpers/convertMinsToHrs.js
@@ -1,4 +1,6 @@
 export default mins => {
+  if (mins < 0) throw new Error('Input must be positive.');
+  if(typeof mins === 'string' || !Number.isInteger(mins)) throw new Error('Input must be an integer.');
   return {
     hrs: Math.floor(mins / 60),
     mins: mins % 60,
